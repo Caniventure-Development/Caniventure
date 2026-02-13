1ddf22eecc775d396073a5b1cb213828feb52b69
Yoshiboi18303 <yoshiboi18303@proton.me>
2026-02-12 21:17:50 -0500

    Make the help command not look as ugly as I do - Fixed that ugly piece of shit, unlike the one I have to see in the mirror.

--- src/subcommands/info/help.ts
+++ src/subcommands/info/help.ts
@@ -1,5 +1,8 @@
-import { stripIndents } from 'common-tags'
-import { Command, Formatter, SubCommand, type CommandContext } from 'seyfert'
+/* eslint-disable @stylistic/object-curly-newline */
+import { titleCase } from '@luca/cases'
+import { Time } from '@sapphire/timestamp'
+import { Command, SubCommand, type CommandContext, type Embed } from 'seyfert'
+import type { CreateComponentCollectorResult } from 'seyfert/lib/components/handler'
 import { BaseBotChatInputSubcommand } from '#subcommands/index.ts'
 
 const getSubcommands = (cmd: Command) =>
@@ -9,32 +12,149 @@ export class HelpSubcommand extends BaseBotChatInputSubcommand {
   public override async run(ctx: CommandContext) {
     await ctx.deferReply()
 
-    const commands = ctx.client.commands.values.filter(
-      (cmd) => cmd instanceof Command
-    )
+    const { ui, utilities } = ctx
 
-    const commandText = commands.map((cmd) => {
-      if (!cmd.options) return
+    const commands = ctx.client.commands.values
+      .filter((cmd) => cmd instanceof Command)
+      .sort((a, b) => a.name.localeCompare(b.name))
+
+    const commandEmbeds = commands.map((cmd) => {
+      if (!cmd.options)
+        return ui.embeds.info(cmd.name, {
+          description: 'No subcommands',
+        })
 
       const subcommandsInCommand = getSubcommands(cmd).sort((a, b) =>
         a.name.localeCompare(b.name)
       )
       const subcommandsLength = subcommandsInCommand.length
 
-      return (
-        stripIndents`
-        ${cmd.name} - ${subcommandsLength} ${subcommandsLength === 1 ? 'command' : 'commands'}
-        ${subcommandsInCommand.map((sub) => `- ${sub.name} (${sub.description})`).join('\n')}
-      ` + '\n'
+      return ui.embeds.info(`${titleCase(cmd.name)} (\`${cmd.name}\`)`, {
+        description: `${subcommandsLength} ${subcommandsLength === 1 ? 'subcommand' : 'subcommands'}`,
+        fields: subcommandsInCommand.map((sub) => ({
+          name: sub.name,
+          value: sub.description,
+        })),
+      })
+    })
+
+    if (commandEmbeds.length === 0) {
+      await ctx.editOrReply({ content: 'There are no commands to show!' })
+      return
+    }
+
+    const startingIndex = 0
+    const startingEmbed = commandEmbeds[startingIndex]!
+    const message = await ctx.editOrReply(
+      {
+        components: [
+          this.buildComponents(ctx, startingIndex, commandEmbeds.length),
+        ],
+        embeds: [startingEmbed],
+      },
+      true
+    )
+
+    const collector = utilities.collectors.create(
+      ctx.interaction,
+      message,
+      'button',
+      {
+        timeout: Time.Minute * 5,
+      }
+    )
+
+    this.handlePagination(ctx, collector, commandEmbeds, startingIndex)
+  }
+
+  private handlePagination(
+    ctx: CommandContext,
+    collector: CreateComponentCollectorResult,
+    embeds: Embed[],
+    pageIndex: number
+  ) {
+    collector.run(this.previousButtonCustomId, async (interaction) => {
+      collector.stop()
+      await interaction.deferUpdate()
+
+      const newIndex = pageIndex - 1
+      if (newIndex < 0) return
+
+      const message = await interaction.editResponse(
+        this.loadPage(ctx, embeds, newIndex)
       )
+
+      const newCollector = ctx.utilities.collectors.create(
+        ctx.interaction,
+        message,
+        'button',
+        {
+          timeout: Time.Minute * 5,
+        }
+      )
+
+      this.handlePagination(ctx, newCollector, embeds, newIndex)
     })
 
-    await ctx.editOrReply({
-      content: stripIndents`
-      **THIS IS TEMPORARY UNTIL MY CREATOR COMES UP WITH A BETTER LOOKING HELP MENU**
+    collector.run(this.nextButtonCustomId, async (interaction) => {
+      collector.stop()
+      await interaction.deferUpdate()
 
-      ${Formatter.codeBlock(commandText.join('\n'))}
-      `,
+      const newIndex = pageIndex + 1
+      if (newIndex >= embeds.length) return
+
+      const message = await interaction.editResponse(
+        this.loadPage(ctx, embeds, newIndex)
+      )
+
+      const newCollector = ctx.utilities.collectors.create(
+        ctx.interaction,
+        message,
+        'button',
+        {
+          timeout: Time.Minute * 5,
+        }
+      )
+
+      this.handlePagination(ctx, newCollector, embeds, newIndex)
     })
+  }
+
+  private loadPage(ctx: CommandContext, embeds: Embed[], newIndex: number) {
+    const page = embeds[newIndex]
+
+    if (!page) throw new Error('Invalid page index ' + newIndex)
+
+    return {
+      components: [this.buildComponents(ctx, newIndex, embeds.length)],
+      embeds: [page],
+    }
+  }
+
+  private buildComponents(
+    ctx: CommandContext,
+    sectionIndex: number,
+    max: number
+  ) {
+    const { ui } = ctx
+
+    return ui.actionRows.multiComponents(
+      ui.buttons.primary(this.previousButtonCustomId, {
+        label: 'Previous',
+        disabled: sectionIndex === 0,
+      }),
+      ui.buttons.primary(this.nextButtonCustomId, {
+        label: 'Next',
+        disabled: sectionIndex + 1 >= max,
+      })
+    )
+  }
+
+  private get previousButtonCustomId() {
+    return 'prev'
+  }
+
+  private get nextButtonCustomId() {
+    return 'next'
   }
 }
